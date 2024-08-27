200f3dfd7921d061f1d788c7cc5c3931350f1e14
sanjana-purbia <sanjana@gkmit.co>
2024-08-27 22:16:52 +0530

    Update

--- vars/ssh_server.groovy
+++ vars/ssh_server.groovy
@@ -1,4 +1,4 @@
 def call(Map config){
   sh  ' chmod 0400 task.pem'
-  sh 'ssh -i task.pem -o StrictHostKeyChecking=no ${config.usrname}@${config.ip} "NUMBER=${BUILD_NUMBER} docker-compose -f ./app/docker-compose.yml up -d" '
+  sh 'ssh -i task.pem -o StrictHostKeyChecking=no ${config.username}@${config.ip} "NUMBER=${BUILD_NUMBER} docker-compose -f ./app/docker-compose.yml up -d" '
 }
