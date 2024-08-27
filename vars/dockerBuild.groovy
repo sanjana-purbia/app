34018cf8e8f157fdacf885a18d8ca10694a23d4c
sanjana-purbia <sanjana@gkmit.co>
2024-08-27 19:39:51 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(Map config){
-  sh "docker build -t config.image:${BUILD_NUMBER} ."
+  sh "docker build -t ${config.image}:${BUILD_NUMBER} ."
   docker.withRegistry( 'https://index.docker.io/v1/', "${config.user}" ){
                         sh "docker push ${config.image}:${BUILD_NUMBER}"
                     }
