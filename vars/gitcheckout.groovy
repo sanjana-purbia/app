7fec74c596d61f1424605dcf041b4845e56af466
sanjana-purbia <sanjana@gkmit.co>
2024-08-27 19:12:33 +0530

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,3 +1,3 @@
-def call(Map,config) {
- checkout scmGit(branches: [[name: 'config.branch']], extensions: [], userRemoteConfigs: [[credentialsId: 'config.id', url: config.url]])
+def call(Map config) {
+ checkout scmGit(branches: [[name: config.branch]], extensions: [], userRemoteConfigs: [[credentialsId: config.id, url: config.url]])
 }
