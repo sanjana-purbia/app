81217cb84ae94b90829c90993d8c1fa157470a87
sanjana-purbia <sanjana@gkmit.co>
2024-08-27 19:00:52 +0530

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,3 +1,3 @@
 def call() {
- sh 'checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github', url: 'https://github.com/sanjana-purbia/app.git']])'
+ checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github', url: 'https://github.com/sanjana-purbia/app.git']])
 }
